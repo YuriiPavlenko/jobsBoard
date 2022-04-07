b4f74ef765ea90a65b553db38e840eea03c5fcbe
Eugene Fursov <fursovjenya01@gmail.com>
2022-04-07 19:04:43 +0300

    change intervals in CSS

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,22 +2,19 @@ import React from "react"
 
 const Card = ({ typ, firma, position, link }) => {
   return (
-    <div className="card">
-      <div className="name-container">
-        <span className="card-name">{position}</span>
+    <a target="_blank" href={link} rel="noreferrer">
+      <div className="card">
+        <div className="name-container">
+          <span className="card-name">{position}</span>
+        </div>
+        <div className="text-otstup">
+          <span className="card-text">Frima: {firma}</span>
+        </div>
+        <div>
+          <span className="card-text">Type: {typ}</span>
+        </div>
       </div>
-      <div>
-        <span className="card-text">Frima: {firma}</span>
-      </div>
-      <div>
-        <span className="card-text">Type: {typ}</span>
-      </div>
-      <div>
-        <a target="_blank" href={link} rel="noreferrer">
-          Link
-        </a>
-      </div>
-    </div>
+    </a>
   )
 }
 
